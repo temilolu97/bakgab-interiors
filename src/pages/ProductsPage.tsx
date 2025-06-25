dd06d6ea1f3ce4225b2b962682af424cbda804b6
temilolu97 <temiloluwaabiodunojo@gmail.com>
2025-06-25 22:41:11 +0100

    update

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -3,7 +3,7 @@ import { products } from '../productsData';
 
 export default function ProductsPage() {
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4 my-10">
       <h2 className="text-3xl font-bold mb-8 text-blue-900">All Products</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {products.map((product) => (
